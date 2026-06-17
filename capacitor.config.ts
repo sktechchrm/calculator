3c291ca26e759c01f030cdf6361a57b7aa8dbee9
saifulislam-74@dipti.com.bd <saifulislam-74@dipti.com.bd>
2026-06-17 12:28:22 +0600

    Updated lot of

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sktechchrm.calculator',
-  appName: 'Mario Calculator',
+  appName: 'Mario Smart Calculator',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/calculator/',
+  base: './',
   publicDir: 'public',
   build: {
     target: 'es2015',
